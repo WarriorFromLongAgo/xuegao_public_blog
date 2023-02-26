995c9e4dfe9cae4abb948676f3aec59ea7d27036
WarriorFromLongAgo <hubeixuegao@gmail.com>
2023-02-26 23:50:33 +0800

    java

--- git-push.bat
+++ git-push.bat
@@ -26,7 +26,3 @@ echo success
 echo;
  
 pause
-
-@REM ————————————————
-@REM 版权声明：本文为CSDN博主「仲夏宁叶香」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
-@REM 原文链接：https://blog.csdn.net/M82_A1/article/details/108350356
